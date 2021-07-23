cee83588766f3b0cda12ca593206a791d50720d2
MTRNord <mtrnord1@gmail.com>
2021-07-23 15:49:13 +0200

    Port https://foss.heptapod.net/magic-lantern/magic-lantern/-/merge_requests/951

--- src/fw-signature.h
+++ src/fw-signature.h
@@ -17,6 +17,7 @@
 #define SIG_7D_203   0x50163E93 // from FF010000
 #define SIG_7D_MASTER_203 0x640BF4D1 // from FF010000
 #define SIG_1100D_105 0x46de7624 // from FF010000
+#define SIG_4000D_100 0x56d44dd6 // from FE0C0000 (atypical)
 #define SIG_6D_116   0x11cb1ed2 // from FF0C0000
 #define SIG_5D3_113  0x2e2f65f5 // from FF0C0000
 #define SIG_5D3_123  0x672EEACE // from FF0C0000
